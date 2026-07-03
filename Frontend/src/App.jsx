3c0810ee582dd8cc3a7c4d81601e39dbe3733309
Monica <monicakonduru1356@gmail.com>
2026-07-03 16:46:40 +0530

    Frontend setup done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-function App() {
-  return (
-    <>
-      <div className="bg-blue-100 min-h-screen flex items-center justify-center text-3xl font-bold">
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
 
-      </div>
-    </>
+function App() {
+
+  return (
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
+    </AuthProvider>
   )
 }
 
